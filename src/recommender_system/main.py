5349093af3258cbaf569bb38d2827bae3b29b459
Kinjuriu <stephanenjokingugi@gmail.com>
2023-05-24 22:22:36 +0300

    fix imports

--- src/recommender_system/main.py
+++ src/recommender_system/main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import os
-from pre_processing import preProcessing
+from pre_processing import pre_process_data
 from utils import get_top_movies
 from recommender import get_recommendations
 from surprise.reader import Reader
@@ -32,7 +32,7 @@ path = FILE_PATH
 ratings = read_csv(path)
 
 # Call preprocessor to clean
-cleaned_ratings = preProcessing(ratings)
+cleaned_ratings = pre_process_data(ratings)
 #return ratings
 
 # Rank-based recommendation systems provide recommendations based on the most popular items
@@ -48,56 +48,49 @@ rating_count = cleaned_ratings.groupby('movieId')['rating'].count()
 final_rating = pd.DataFrame({'avg_rating': avg_rating, 'rating_count': rating_count})
 
 if __name__ == "__main__":
-    cleaned_ratings = load_data()
-    
-    print(recommend_movies_based_on_popularity(cleaned_ratings, 5, 50))
-    print(recommend_movies_based_on_popularity(cleaned_ratings, 5, 100))
-    
-    recommend_movies_based_on_similarity(cleaned_ratings, 5, 4)
+    # Recommend top 5 movies with 50 minimum interactions based on popularity
+    print(list(get_top_movies(final_rating, 5, 50)))
 
-# Recommend top 5 movies with 50 minimum interactions based on popularity
-print(list(get_top_movies(final_rating, 5, 50)))
-
-# Recommend top 5 movies with 100 minimum interactions based on popularity
-print(list(get_top_movies(final_rating, 5, 100)))
+    # Recommend top 5 movies with 100 minimum interactions based on popularity
+    print(list(get_top_movies(final_rating, 5, 100)))
 
 
-# Set the rating scale for the reader
-reader = Reader(rating_scale=(0, 5))
+    # Set the rating scale for the reader
+    reader = Reader(rating_scale=(0, 5))
 
-# Load the ratings data
-data = Dataset.load_from_df(ratings[['userId', 'movieId', 'rating']], reader)
+    # Load the ratings data
+    data = Dataset.load_from_df(ratings[['userId', 'movieId', 'rating']], reader)
 
-# Split the data into train and test sets
-trainset, testset = train_test_split(data, test_size=0.2, random_state=42)
+    # Split the data into train and test sets
+    trainset, testset = train_test_split(data, test_size=0.2, random_state=42)
 
-# Set the options for the similarity measure
-sim_options = {'name': 'cosine', 'user_based': True}
+    # Set the options for the similarity measure
+    sim_options = {'name': 'cosine', 'user_based': True}
 
-# Use the optimal similarity measure for user-user collaborative filtering
-# Create an instance of KNNBasic with the optimal hyperparameters
-similarity_algo_optimized_user = KNNBasic(
-    sim_options=sim_options, k=40, min_k=6, verbose=False
-)
+    # Use the optimal similarity measure for user-user collaborative filtering
+    # Create an instance of KNNBasic with the optimal hyperparameters
+    similarity_algo_optimized_user = KNNBasic(
+        sim_options=sim_options, k=40, min_k=6, verbose=False
+    )
 
-# Train the algorithm on the trainset
-similarity_algo_optimized_user.fit(trainset)
+    # Train the algorithm on the trainset
+    similarity_algo_optimized_user.fit(trainset)
 
 
-# Set the user ID and number of movies to recommend
-user_id = 4
-top_movies = 5
+    # Set the user ID and number of movies to recommend
+    user_id = 4
+    top_movies = 5
 
-# Get recommendations using the similarity-based recommendation system
-recommendations = get_recommendations(
-    ratings, top_movies, user_id, similarity_algo_optimized_user
-)
+    # Get recommendations using the similarity-based recommendation system
+    recommendations = get_recommendations(
+        ratings, top_movies, user_id, similarity_algo_optimized_user
+    )
 
-# Convert recommendations to a dataframe
-recommendations_df = pd.DataFrame(recommendations)
+    # Convert recommendations to a dataframe
+    recommendations_df = pd.DataFrame(recommendations)
 
-# Create a filename using the user ID and number of movies
-filename = f'recommendations_userId_{user_id}_for_top_{top_movies}_movies.csv'
+    # Create a filename using the user ID and number of movies
+    filename = f'recommendations_userId_{user_id}_for_top_{top_movies}_movies.csv'
 
-# Save the recommendations to a CSV file
-recommendations_df.to_csv(filename, index=False)
+    # Save the recommendations to a CSV file
+    recommendations_df.to_csv(filename, index=False)
